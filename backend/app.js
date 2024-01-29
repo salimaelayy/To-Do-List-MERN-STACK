fa498c69fe58afb55668342824056a9ce2b116cb
salimaelayy <64556840+salimaelayy@users.noreply.github.com>
2024-01-29 14:56:16 +0100

    connection

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,8 @@
 const express= require("express")
 require('dotenv').config();
+require('./connection')
 const app=express()
-
-// Connect to the database
-mongoose.connect("mongodb://localhost:27017/ToDoListDB")
-  .then(() => {
-    console.log("Connected to the database");
-  })
-  .catch((err) => {
-    console.log("Not connected to the database " + err);
-  });
 
 app.listen(process.env.PORT, () => {
     console.log('Server is running on port', process.env.PORT)
 })
-
--- backend/connection.js
+++ backend/connection.js
@@ -1,8 +1,5 @@
 require('dotenv').config();
-const express = require('express');
 const mongoose = require('mongoose');
-
-const app = express();
 
 // Connect to the database
 mongoose.connect("mongodb://localhost:27017/ToDoListDB")
@@ -13,8 +10,4 @@ mongoose.connect("mongodb://localhost:27017/ToDoListDB")
     console.log("Not connected to the database " + err);
   });
 
-
-// app.listen(process.env.PORT, () => {
-//     console.log('Server is running on port', process.env.PORT)
-// })
 
