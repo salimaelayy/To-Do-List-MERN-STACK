fb9398ac149f001b8122def5e7a7c289a6e6e20e
salimaelayy <64556840+salimaelayy@users.noreply.github.com>
2024-01-31 15:48:43 +0100

    staged3

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,8 @@
 const express= require("express")
 const cookieParser = require('cookie-parser')
 const userRouter=require('./Routes/UserRouter')
-const mongoose = require("mongoose");
-
 require('dotenv').config();
 require('./connection')
-require('./connection')
-const router = require('./Routes/TaskRouter');
-// const swaggerDocs =  require('./Docs/Swagger.js'); 
-
 const app=express()
 app.use(express.json());
 app.use(cookieParser())
@@ -21,12 +15,6 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use('/users', userRouter);
-app.use(express.json());
- 
-// swaggerDocs(app, 3000);
-app.use('/', router);
-
-
 
 app.listen(process.env.PORT, () => {
     console.log('Server is running on port', process.env.PORT)
