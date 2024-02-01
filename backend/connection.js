7f398e334af552545d429531d332732179345972
salimaelayy <64556840+salimaelayy@users.noreply.github.com>
2024-02-01 13:16:28 +0100

    concurrently

--- backend/connection.js
+++ backend/connection.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-require('dotenv').config();
-const mongoose = require('mongoose');
-=======
 //connection.js
 
 require('dotenv').config();
 const mongoose = require('mongoose'); 
->>>>>>> cca716ebb48a7131cb6102a666c92857ab6cd542
 
 // Connect to the database
 mongoose.connect(process.env.URI)
@@ -17,7 +12,3 @@ mongoose.connect(process.env.URI)
     console.log("Not connected to the database " + err);
   });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> cca716ebb48a7131cb6102a666c92857ab6cd542
